52b37839067ad89b67f580697705dce6d9a78291
dlikane <dlikane@gmail.com>
2016-12-04 22:09:43 +1100

    wip

--- app/containers/TestPage/index.js
+++ app/containers/TestPage/index.js
@@ -11,6 +11,7 @@ import selectTestPage from './selectors';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 import Input from './Input';
+import { SplitButton, MenuItem } from 'react-bootstrap';
 
 export class TestPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
@@ -18,6 +19,13 @@ export class TestPage extends React.PureComponent { // eslint-disable-line react
     return (
       <div>
         <FormattedMessage {...messages.header} />
+        <SplitButton title="Dropdown right" pullRight id="split-button-pull-right">
+          <MenuItem eventKey="1">Action</MenuItem>
+          <MenuItem eventKey="2">Another action</MenuItem>
+          <MenuItem eventKey="3">Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey="4">Separated link</MenuItem>
+        </SplitButton>
         <Input
           id="originKind"
           type="text"
