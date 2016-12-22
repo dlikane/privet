d7c61dbddf400709c03cbb6f0f4e6ae52f9d0d80
Dmitry Likane <dmitry.likane@nab.com.au>
2016-12-22 16:31:29 +1100

    wip

--- app/containers/TestPage/index.js
+++ app/containers/TestPage/index.js
@@ -2,14 +2,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PostList from './PostList'
+import { generateRandomList } from './demo'
+import Immutable from 'immutable';
 
 //
 // https://github.com/bvaughn/react-virtualized/blob/master/source/List/List.example.js
 
+const list = Immutable.List(generateRandomList());
+
 class TestPage extends React.Component {
+
   render() {
+
     return (
-      <PostList/>
+      <PostList list={list} />
     );
   }
 }
